37b69a3f64d0bd6d1c7e30f90642158807b7f04f
Yesola <dldPthf421@gmail.com>
2019-11-19 20:07:59 +0900

    clean the project structure

--- README.md
+++
@@ -1,2 +0,0 @@
-# counselApp_node_js
-상담앱을 위한 node.js 서버
--- web1/node/fileread.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs'); // const(상수), var로 해도 문제없음
-fs.readFile('node/sample.txt','utf8', (err, data) => {
-    if(err) throw err;
-    console.log(data);
-});
-
